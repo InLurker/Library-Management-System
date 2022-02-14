c96bc43323c9b46a139f760e9b3a265a6f1781b6
sutardjik <77554538+sutardjik@users.noreply.github.com>
2022-02-13 23:09:53 -0800

    Update Book.h

--- Library Management System/Library Management System/Book.h
+++ Library Management System/Library Management System/Book.h
@@ -15,7 +15,7 @@ public:
     std::string publisher;
     std::string status;
     std::vector <std::string> borrower_record;
-    void addToDatabase();
+    void addToDatabase(std::string filename);
     void borrowBook(std::string name, std::string phoneNum);
     void reserveBook(std::string name, std::string phoneNum);
     void returnBook();
